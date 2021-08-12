9185e872eb15fa5824af3ef5841f08e2d38a5484
anshumanbisht16 <85048519+anshumanbisht16@users.noreply.github.com>
2021-08-12 22:03:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function haha(){
+var result=Number(document.getElementById("input1").value)+
+Number(document.getElementById("input2").value);
+window.alert(result);
+    }
+function haha(){
+var result=Number(document.getElementById("input1").value)-
+    Number(document.getElementById("input2").value);
+window.alert(result);
+}
